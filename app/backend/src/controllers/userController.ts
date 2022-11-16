11f809e8fbfbf22aec1bf7ef00da92e9dc94187d
matheusnff85 <math.marinho154@gmail.com>
2022-11-16 17:55:03 -0300

    feat: funcao de login e controller de user

---
+++ app/backend/src/controllers/userController.ts
@@ -0,0 +1,55 @@
+import { RequestHandler } from 'express';
+import UserServices from '../services/userServices';
+import { IUser } from '../interfaces/UserInterface';
+import StatusCodes from '../types/statusCodes';
+
+export default class UserController {
+  constructor(private userServices: UserServices = new UserServices()) {}
+
+  public findAll: RequestHandler = async (_req, res, next) => {
+    try {
+      const result = await this.userServices.findAll();
+      return res.status(StatusCodes.OK).json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public findOne: RequestHandler = async (req, res, next) => {
+    try {
+      const result = await this.userServices.findOne(req.params.id);
+      return result;
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public login: RequestHandler = async (req, res, next) => {
+    try {
+      const result = await this.userServices.login(req.body);
+      return res.status(StatusCodes.OK).json({ token: result });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public create: RequestHandler = async (req, res, next) => {
+    try {
+      const result = await this.userServices.create(req.body);
+      return res.status(StatusCodes.CREATED).json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public update: RequestHandler = async (req, res, next) => {
+    try {
+      const userObj: IUser = req.body;
+      userObj.id = req.params.id;
+      const result = await this.userServices.update(userObj);
+      return result;
+    } catch (error) {
+      next(error);
+    }
+  };
+}
--- app/backend/src/services/userServices.ts
+++ app/backend/src/services/userServices.ts
@@ -1,8 +1,10 @@
+import * as bcrypt from 'bcryptjs';
 import CustomError from '../types/customError';
 import StatusCodes from '../types/statusCodes';
 import { IUser } from '../interfaces/UserInterface';
 import UsersModel from '../models/UsersModel';
 import { userSchema, validateNewUser } from '../validations/userValidation';
+import generateToken from '../utils/generateToken';
 
 export default class UserServices {
   constructor(private usersModel: UsersModel = new UsersModel()) {}
@@ -16,6 +18,16 @@ export default class UserServices {
     const result = await this.usersModel.findOne(id);
     if (!result) throw new CustomError(StatusCodes.NOT_FOUND, 'User not found.');
     return result;
+  }
+
+  public async login(loginObj: IUser): Promise<string> {
+    const { username, password } = loginObj;
+    const result = await this.usersModel.findByUsername(username);
+    if (!result) throw new CustomError(StatusCodes.UNAUTHORIZED, 'incorrect username');
+    const validatePass = await bcrypt.compare(password, result.password);
+    if (!validatePass) throw new CustomError(StatusCodes.UNAUTHORIZED, 'incorrect password');
+    const token = generateToken(username);
+    return token;
   }
 
   public async create(newUser: IUser): Promise<IUser> {
