fd2c5ca9136c43bd2ffcec0fd79609a609633031
matheusnff85 <math.marinho154@gmail.com>
2022-11-21 01:57:12 -0300

    fix: corrige retorno da funcao

--- app/backend/src/models/UsersModel.ts
+++ app/backend/src/models/UsersModel.ts
@@ -36,7 +36,6 @@ export default class UsersModel {
       include: [
         { model: Accounts, as: 'userBalance', attributes: ['balance'] },
       ],
-      attributes: { exclude: ['password'] },
     });
     return result;
   }
